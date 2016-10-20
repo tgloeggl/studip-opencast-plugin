0944ee9c539ca20974f62ee84dea368487f1a0c6
André <andre@gundk.it>
2016-10-20 13:52:43 +0200

    fix migrations

--- migrations/007_add_configid_column.php
+++ migrations/007_add_configid_column.php
@@ -1,15 +1,10 @@
 <?php
 
-class AddLogActions extends Migration {
+class AddConfigidColumn extends Migration {
 
     function up()
     {
-
-
-
-
-
-        DBManager::get()->query("ALTER TABLE `oc_config` DROP PRIMARY KEY, ADD UNIQUE KEY(service_url,service_user,service_password");
+        DBManager::get()->query("ALTER TABLE `oc_config` DROP PRIMARY KEY, ADD UNIQUE KEY(service_url,service_user,service_password);");
         DBManager::get()->query("ALTER TABLE `oc_config` ADD `config_id` INT UNIQUE KEY NOT NULL AUTO_INCREMENT FIRST;");
         DBManager::get()->query("ALTER TABLE `oc_endpoints` ADD `config_id` INT NOT NULL DEFAULT 1 FIRST;");
         DBManager::get()->query("ALTER TABLE `oc_resources` ADD `config_id` INT NOT NULL DEFAULT 1 FIRST;");
@@ -22,12 +17,12 @@ class AddLogActions extends Migration {
 
     function down()
     {
-        DBManager::get()->query("ALTER TABLE `oc_config` DROP COLUMN `config_id`;")
-        DBManager::get()->query("ALTER TABLE `oc_config` DROP INDEX `service_url`,  ADD PRIMARY KEY (`service_url`);")
-        DBManager::get()->query("ALTER TABLE `oc_endpoints` DROP COLUMN `config_id`;")
-        DBManager::get()->query("ALTER TABLE `oc_resources` DROP COLUMN `config_id`;")
-        DBManager::get()->query("ALTER TABLE `oc_seminar_series` DROP COLUMN `config_id`;")
-        DBManager::get()->query("ALTER TABLE `oc_seminar_workflows` DROP COLUMN `config_id`;")
+        DBManager::get()->query("ALTER TABLE `oc_config` DROP COLUMN `config_id`;");
+        DBManager::get()->query("ALTER TABLE `oc_config` DROP INDEX `service_url`,  ADD PRIMARY KEY (`service_url`);");
+        DBManager::get()->query("ALTER TABLE `oc_endpoints` DROP COLUMN `config_id`;");
+        DBManager::get()->query("ALTER TABLE `oc_resources` DROP COLUMN `config_id`;");
+        DBManager::get()->query("ALTER TABLE `oc_seminar_series` DROP COLUMN `config_id`;");
+        DBManager::get()->query("ALTER TABLE `oc_seminar_workflows` DROP COLUMN `config_id`;");
 
     }
 }
